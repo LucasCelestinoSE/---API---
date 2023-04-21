7fb2e5b9fa58959e6c3b497d1aaf0e4e249bdf05
Lucas Celestino <lucas.celp1@gmail.com>
2023-04-21 15:35:26 -0300

    adicionando cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express'
 import { AppDataSource } from './data-source'
 import routes from './routes'
-
+import cors from 'cors'
 AppDataSource.initialize().then(() => {
 	const app = express()
 
 	app.use(express.json())
-
+	app.use((req,res,next) =>{
+		res.header("Access-Control-Allow-Origin", "*")
+		app.use(cors());
+		next()
+	})
+	
 	app.use(routes)
 
 	return app.listen(process.env.PORT)
